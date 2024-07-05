bb50dd62587c7e6a7a0428bcfa847851344567a3
L1x (AKA Nathan J.) <nathan.jeannot@epitech.eu>
2024-07-06 00:01:23 +0200

    refactor(Exception): renamed TeamSizeException to TeamFullException

---
+++ src/main/java/fr/hashtek/spigot/hashboard/exceptions/TeamFullException.java
@@ -0,0 +1,11 @@
+package fr.hashtek.spigot.hashboard.exceptions;
+
+public class TeamFullException extends Exception
+{
+
+    public TeamFullException(String message)
+    {
+        super(message);
+    }
+
+}
