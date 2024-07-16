3896845187200eb4d1d1d49a41f80e0969cee756
L1x (AKA Nathan J.) <nathan.jeannot@epitech.eu>
2024-07-16 02:08:24 +0200

    refactor(HashTeam): use a TeamData instead of storing directly the team data inside the HashTeam class

--- src/main/java/fr/hashtek/spigot/hashboard/teams/HashTeam.java
+++ src/main/java/fr/hashtek/spigot/hashboard/teams/HashTeam.java
@@ -4,11 +4,14 @@ import fr.hashtek.hashutils.Reflection;
 import fr.hashtek.spigot.hashboard.exceptions.AlreadyInTeamException;
 import fr.hashtek.spigot.hashboard.exceptions.NotInTeamException;
 import fr.hashtek.spigot.hashboard.exceptions.TeamFullException;
-import fr.hashtek.spigot.hashboard.packets.teams.*;
+//import fr.hashtek.spigot.hashboard.packets.teams.;
+import fr.hashtek.spigot.hashboard.packets.teams.PacketTeamFlags;
+import fr.hashtek.spigot.hashboard.packets.teams.PacketTeamManager;
+import fr.hashtek.spigot.hashboard.packets.teams.PacketTeamMode;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class HashTeam extends Reflection
@@ -16,30 +19,16 @@ public class HashTeam extends Reflection
 
     private final PacketTeamManager packetTeamManager;
 
-    private final ArrayDeque<String> currentPlayers;
-    private final ArrayDeque<String> addedPlayersQueue;
-    private final ArrayDeque<String> removedPlayersQueue;
     private final HashMap<Player, Boolean> receivers;
 
-    private final ArrayList<PacketTeamFlags> flags = new ArrayList<PacketTeamFlags>();
-    private final String id;
-    private final int size;
-
-    private TeamColor color;
-    private String prefix;
-    private String suffix;
+    private final TeamData teamData;
 
     public HashTeam(String id, TeamColor color, int size)
             throws Exception
     {
         this.packetTeamManager = new PacketTeamManager(id);
-        this.id = id;
-        this.size = size;
-        this.currentPlayers = new ArrayDeque<String>();
-        this.addedPlayersQueue = new ArrayDeque<String>();
-        this.removedPlayersQueue = new ArrayDeque<String>();
+        this.teamData = new TeamData(id, color, size);
         this.receivers = new HashMap<Player, Boolean>();
-        this.color = color;
     }
 
     /**
@@ -48,84 +37,136 @@ public class HashTeam extends Reflection
      * all the changes (added / removed players) will not be updated yet. To fix this, you can filter
      * the different lists to check.
      *
-     * @param player The player to search for.
-     * @param checkCurrentPlayers Search for the current players before calling the {@link HashTeam#update} method.
-     * @param checkAddedPlayers Search for the added players before calling the {@link HashTeam#update} method.
-     * @param checkRemovedPlayers Search for the removed players before calling the {@link HashTeam#update} method.
-     * @return {@code true} if the player has been found, {@code false} otherwise.
+     * @param       player                  The player to search for.
+     * @param       checkCurrentPlayers     Search for the current players before calling the {@link HashTeam#update} method.
+     * @param       checkAddedPlayers       Search for the added players before calling the {@link HashTeam#update} method.
+     * @param       checkRemovedPlayers     Search for the removed players before calling the {@link HashTeam#update} method.
+     * @return                              {@code true} if the player has been found, {@code false} otherwise.
      */
     public boolean containsPlayer(Player player, boolean checkCurrentPlayers, boolean checkAddedPlayers, boolean checkRemovedPlayers)
     {
-        final String playerName = player.getName();
-        final ArrayList<String> playersToCheck = new ArrayList<>();
-
-        if (checkAddedPlayers)
-            playersToCheck.addAll(this.addedPlayersQueue);
-        if (checkRemovedPlayers)
-            playersToCheck.addAll(this.removedPlayersQueue);
-        if (checkCurrentPlayers)
-            playersToCheck.addAll(this.currentPlayers);
-
-        return playersToCheck.contains(playerName);
+        return this.teamData.containsPlayer(player, checkCurrentPlayers, checkAddedPlayers, checkRemovedPlayers);
     }
 
     /**
      * Add a player to the team.
      * {@code NOTE} - Don't forget to call the {@link HashTeam#update} method to take changes into account.
      *
-     * @param player The player to add to the team.
-     * @throws TeamFullException If the team is full.
-     * @throws AlreadyInTeamException If the player is already in the team.
+     * @param       player                  The player to add to the team.
+     * @throws      TeamFullException       If the team is full.
+     * @throws      AlreadyInTeamException  If the player is already in the team.
+     * @return                              The team itself.
      */
-    public void addPlayer(Player player)
+    public HashTeam addPlayer(Player player)
             throws TeamFullException, AlreadyInTeamException
     {
-        final String playerUsername = player.getName();
-
-        if (this.addedPlayersQueue.contains(playerUsername))
-            return;
-
-        if (this.size != 0 && this.currentPlayers.size() >= this.size)
-            throw new TeamFullException("The team \"" + this.id + "\" is full.");
-        if (this.currentPlayers.contains(playerUsername))
-            throw new AlreadyInTeamException("The player \"" + playerUsername + "\" is already in the team \"" + this.id + "\".");
-
-        if (!this.removedPlayersQueue.contains(playerUsername))
-            this.addedPlayersQueue.add(playerUsername);
-        else
-            this.removedPlayersQueue.remove(playerUsername);
+        this.teamData.addPlayer(player);
+        return this;
     }
 
     /**
      * Remove a player from the team.
      * {@code NOTE} - Don't forget to call the {@link HashTeam#update} method to take changes into account.
      *
-     * @param player The player to add to the team.
-     * @throws NotInTeamException If the player is not in the team.
+     * @param       player              The player to add to the team.
+     * @throws      NotInTeamException  If the player is not in the team.
+     * @return                          The team itself.
      */
-    public void removePlayer(Player player)
+    public HashTeam removePlayer(Player player)
             throws NotInTeamException
     {
-        final String playerUsername = player.getName();
+        this.teamData.removePlayer(player);
+        return this;
+    }
 
-        if (this.removedPlayersQueue.contains(playerUsername))
-            return;
+    public void addReceiver(Player player)
+            throws Exception
+    {
+        this.receivers.put(player, false);
+        this.create(player);
+    }
 
-        if (!this.currentPlayers.contains(playerUsername))
-            throw new NotInTeamException("The player \"" + playerUsername + "\" is not in the team \"" + this.id + "\".");
+    public void removeReceiver(Player player)
+            throws Exception
+    {
+        this.receivers.remove(player);
+        this.delete(player);
+    }
 
-        if (!this.addedPlayersQueue.contains(playerUsername))
-            this.removedPlayersQueue.add(playerUsername);
-        else
-            this.addedPlayersQueue.remove(playerUsername);
+    public void update()
+            throws Exception
+    {
+        for (Player player : this.receivers.keySet()) {
+            if (!this.receivers.get(player)) {
+                this.create(player);
+                continue;
+            }
+            this.update(player);
+        }
+        this.teamData.validateChanges();
+    }
+
+    private void update(Player player)
+            throws Exception
+    {
+        ArrayList<String> addedPlayers = this.teamData.getAddedPlayers();
+        ArrayList<String> removedPlayers = this.teamData.getRemovedPlayers();
+
+        if (!addedPlayers.isEmpty())
+            this.updateAddedPlayers(player);
+        if (!removedPlayers.isEmpty())
+            this.updateRemovedPlayers(player);
+        if (this.teamData.checkIfHasChanged())
+            this.updateTeamInformation(player);
+    }
+
+    private void updateAddedPlayers(Player player)
+            throws Exception
+    {
+        final Object packet = this.packetTeamManager.PacketPlayOutScoreboardTeam(PacketTeamMode.ADD_PLAYER, this.teamData.getAddedPlayers());
+
+        this.sendPacket(player, packet);
+    }
+
+    private void updateRemovedPlayers(Player player)
+            throws Exception
+    {
+        final Object packet = this.packetTeamManager.PacketPlayOutScoreboardTeam(PacketTeamMode.ADD_PLAYER, this.teamData.getRemovedPlayers());
+
+        this.sendPacket(player, packet);
+
+    }
+
+    private void updateTeamInformation(Player player)
+            throws Exception
+    {
+        final TeamTagVisibility visibility = this.teamData.getTagVisibility();
+        final TeamColor color = this.teamData.getColor();
+        final String prefix = this.teamData.getPrefix();
+        final String suffix = this.teamData.getSuffix();
+        final byte flags = this.teamData.getFlags();
+
+        final Object packet = this.packetTeamManager.PacketPlayOutScoreboardTeam(
+            PacketTeamMode.UPDATE_TEAM_INFO, visibility, color,
+            prefix, suffix, Collections.emptyList(), flags
+        );
+
+        this.sendPacket(player, packet);
     }
 
     private void create(Player player)
             throws Exception
     {
+        final TeamTagVisibility visibility = this.teamData.getTagVisibility();
+        final TeamColor color = this.teamData.getColor();
+        final String prefix = this.teamData.getPrefix();
+        final String suffix = this.teamData.getSuffix();
+        final ArrayList<String> currentPlayers = this.teamData.getCurrentPlayers();
+        final byte flags = this.teamData.getFlags();
+
         final Object packet = this.packetTeamManager.PacketPlayOutScoreboardTeam(
-            PacketTeamMode.CREATE_TEAM, PacketTeamTagVisibility.VISIBLE, this.color,
-            this.prefix, this.suffix, this.currentPlayers, this.flags
+                PacketTeamMode.CREATE_TEAM, visibility, color,
+                prefix, suffix, currentPlayers, flags
         );
 
         this.sendPacket(player, packet);
